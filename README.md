778be83586566956243d1e1250cfa13ae7d215a9
Roberto Faga <robertofaga@gmail.com>
2013-11-17 20:25:20 -0200

    Sending project to gh

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 raspcontrol
 ===========
+
+This is a small project to control my Raspberry XBMC unit.
+
+It's a simple Flask server to start/stop Transmission daemon and XBMC daemon. Also, it has a Reboot linux system button.
+
+I'm using Jquery Mobile and added a link in my phone, so I can control it easily.
---
+++ server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template, redirect
+import subprocess
+def run(command):
+    try:
+        return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        return "Error: command doesn't exist!"
+    
+    
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    xbmc = "xbmc start/running" in run("service xbmc status")
+    transmission = "daemon is running" in run("service transmission-daemon status")
+    return render_template("index.html", xbmc=xbmc, transmission=transmission)
+
+
+@app.route('/xbmcstart')
+def xbmc_start():
+    run("service xbmc restart")
+    run("service xbmc start")
+    return redirect('/')
+
+@app.route('/xbmcstop')
+def xbmc_stop():
+    run("service xbmc stop")
+    return redirect('/')
+
+@app.route('/transtop')
+def transmission_stop():
+    run("service transmission-daemon stop")
+    return redirect('/')
+
+@app.route('/transtart')
+def transmission_start():
+    run("service transmission-daemon start")
+    return redirect('/')
+
+@app.route('/reboot')
+def system_reboot():
+    run("reboot")
+    return redirect('/')
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=56789)
